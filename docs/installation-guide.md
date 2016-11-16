e08e259cb67998ebef615f41623d6e9376dc8a90
Wasim <wasim.sayyed@cuelogic.com>
2016-11-16 17:41:57 +0530

    Added initial files

---
+++ docs/installation-guide.md
@@ -0,0 +1,92 @@
+# Installation Guide
+
+> This page explains how to install RTCMultiConnection v3.
+
+* Youtube Video: [NPM install](https://www.youtube.com/watch?v=EtsiYEW_T8Y) or [Other Videos](https://www.youtube.com/watch?v=EtsiYEW_T8Y&list=PLPRQUXAnRydKdyun-vjKPMrySoow2N4tl)
+
+Or fetch latest code via github:
+
+```sh
+git clone https://github.com/muaz-khan/RTCMultiConnection.git ./RTCMultiConnection
+cd RTCMultiConnection
+sudo npm install --save-dev
+```
+
+To TEST:
+
+```sh
+node server.js
+```
+
+## Stop Old Processes
+
+Check all processes running on port `9001` and stop process by `id`:
+
+```sh
+lsof -n -i4TCP:9001 | grep LISTEN
+kill process-ID
+```
+
+Or stop all processes on a specific port. (It may require `sudo` privileges):
+
+```sh
+fuser -vk 9001/tcp
+```
+
+Now open: `http://localhost:9001/`
+
+## Keep running server in background
+
+```sh
+nohup nodejs server.js > /dev/null 2>&1 &
+```
+
+Or:
+
+```sh
+nohup nodejs server.js &
+```
+
+Or use `forever`:
+
+```sh
+npm install forever -g
+forever start server.js
+```
+
+To auto-start `server.js` on system-reboot (i.e. when Mac/Linux system shuts down or reboots):
+
+```sh
+npm install forever-service
+
+cd __path to your npm install__
+forever-service install ncustomAppName --script server.js
+```
+
+Commands to interact with `service ncustomAppName Start`:
+
+```
+- "sudo service ncustomAppName start" Stop
+- "sudo service ncustomAppName stop" Status
+- "sudo service ncustomAppName status" Restart - "sudo service ncustomAppName restart"
+```
+
+More info about `forever-service` [here](http://stackoverflow.com/a/36027516/552182).
+
+# Other Documents
+
+1. [Getting Started guide for RTCMultiConnection](https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/getting-started.md)
+2. [Installation Guide](https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/installation-guide.md)
+3. [How to Use?](https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/how-to-use.md)
+4. [API Reference](https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/api.md)
+5. [Upgrade from v2 to v3](https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/upgrade.md)
+6. [How to write iOS/Android applications?](https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/ios-android.md)
+7. [Tips & Tricks](https://github.com/muaz-khan/RTCMultiConnection/blob/master/docs/tips-tricks.md)
+
+## Twitter
+
+* https://twitter.com/WebRTCWeb i.e. @WebRTCWeb
+
+## License
+
+[RTCMultiConnection](https://github.com/muaz-khan/RTCMultiConnection) is released under [MIT licence](https://github.com/muaz-khan/RTCMultiConnection/blob/master/LICENSE.md) . Copyright (c) [Muaz Khan](http://www.MuazKhan.com/).
---
+++ server.js
@@ -0,0 +1,202 @@
+// Please use HTTPs on non-localhost domains.
+var isUseHTTPs = false && !(!!process.env.PORT || !!process.env.IP);
+
+// user-guide: change port via "config.json"
+var port = process.env.PORT || 9001;
+
+var autoRebootServerOnFailure = false;
+
+try {
+    var config = require('./config.json');
+
+    if ((config.port || '').toString() !== '9001') {
+        port = parseInt(config.port);
+    }
+
+    if ((config.autoRebootServerOnFailure || '').toString() !== true) {
+        autoRebootServerOnFailure = true;
+    }
+} catch (e) {}
+
+var server = require(isUseHTTPs ? 'https' : 'http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+
+function serverHandler(request, response) {
+    try {
+        var uri = url.parse(request.url).pathname,
+            filename = path.join(process.cwd(), uri);
+
+        if (filename && filename.search(/server.js|Scalable-Broadcast.js|Signaling-Server.js/g) !== -1) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+            response.end();
+            return;
+        }
+
+        var stats;
+
+        try {
+            stats = fs.lstatSync(filename);
+
+            if (filename && filename.search(/demos/g) === -1 && stats.isDirectory()) {
+                response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                response.write('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0;url=Video-Conferencing.html"></head><body></body></html>');
+                response.end();
+                return;
+            }
+        } catch (e) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) {
+            response.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+
+            if (filename.indexOf('/demos/MultiRTC/') !== -1) {
+                filename = filename.replace('/demos/MultiRTC/', '');
+                filename += 'Video-Conferencing.html';
+            } else if (filename.indexOf('/demos/') !== -1) {
+                filename = filename.replace('/demos/', '');
+                filename += 'Video-Conferencing.html';
+            } else {
+                filename += 'Video-Conferencing.html';
+            }
+        }
+
+
+        fs.readFile(filename, 'utf8', function(err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+                response.end();
+                return;
+            }
+
+            try {
+                var demos = (fs.readdirSync('demos') || []);
+
+                if (demos.length) {
+                    var h2 = '<h2 style="text-align:center;display:block;"><a href="https://www.npmjs.com/package/rtcmulticonnection-v3"><img src="https://img.shields.io/npm/v/rtcmulticonnection-v3.svg"></a><a href="https://www.npmjs.com/package/rtcmulticonnection-v3"><img src="https://img.shields.io/npm/dm/rtcmulticonnection-v3.svg"></a><a href="https://travis-ci.org/muaz-khan/RTCMultiConnection"><img src="https://travis-ci.org/muaz-khan/RTCMultiConnection.png?branch=master"></a></h2>';
+                    var otherDemos = '<section class="experiment" id="demos"><details><summary style="text-align:center;">Check ' + (demos.length - 1) + ' other RTCMultiConnection-v3 demos</summary>' + h2 + '<ol>';
+                    demos.forEach(function(f) {
+                        if (f && f !== 'index.html' && f.indexOf('.html') !== -1) {
+                            otherDemos += '<li><a href="/demos/' + f + '">' + f + '</a> (<a href="https://github.com/muaz-khan/RTCMultiConnection/tree/master/demos/' + f + '">Source</a>)</li>';
+                        }
+                    });
+                    otherDemos += '<ol></details></section><section class="experiment own-widgets latest-commits">';
+
+                    file = file.replace('<section class="experiment own-widgets latest-commits">', otherDemos);
+                }
+            } catch (e) {}
+
+            try {
+                var docs = (fs.readdirSync('docs') || []);
+
+                if (docs.length) {
+                    var html = '<section class="experiment" id="docs">';
+                    html += '<details><summary style="text-align:center;">RTCMultiConnection Docs</summary>';
+                    html += '<h2 style="text-align:center;display:block;"><a href="http://www.rtcmulticonnection.org/docs/">http://www.rtcmulticonnection.org/docs/</a></h2>';
+                    html += '<ol>';
+
+                    docs.forEach(function(f) {
+                        if (f.indexOf('DS_Store') == -1) {
+                            html += '<li><a href="https://github.com/muaz-khan/RTCMultiConnection/tree/master/docs/' + f + '">' + f + '</a></li>';
+                        }
+                    });
+
+                    html += '</ol></details></section><section class="experiment own-widgets latest-commits">';
+
+                    file = file.replace('<section class="experiment own-widgets latest-commits">', html);
+                }
+            } catch (e) {}
+
+            response.writeHead(200);
+            response.write(file, 'utf8');
+            response.end();
+        });
+    } catch (e) {
+        response.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        response.write('<h1>Unexpected error:</h1><br><br>' + e.stack || e.message || JSON.stringify(e));
+        response.end();
+    }
+}
+
+var app;
+
+if (isUseHTTPs) {
+    var options = {
+        key: fs.readFileSync(path.join(__dirname, 'fake-keys/privatekey.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'fake-keys/certificate.pem'))
+    };
+    app = server.createServer(options, serverHandler);
+} else app = server.createServer(serverHandler);
+
+function runServer() {
+    app = app.listen(port, process.env.IP || '0.0.0.0', function() {
+        var addr = app.address();
+
+        if (addr.address === '0.0.0.0') {
+            addr.address = 'localhost';
+        }
+
+        console.log('Server listening at ' + (isUseHTTPs ? 'https' : 'http') + '://' + addr.address + ':' + addr.port);
+    });
+
+    require('./Signaling-Server.js')(app, function(socket) {
+        try {
+            var params = socket.handshake.query;
+
+            // "socket" object is totally in your own hands!
+            // do whatever you want!
+
+            // in your HTML page, you can access socket as following:
+            // connection.socketCustomEvent = 'custom-message';
+            // var socket = connection.getSocket();
+            // socket.emit(connection.socketCustomEvent, { test: true });
+
+            if (!params.socketCustomEvent) {
+                params.socketCustomEvent = 'custom-message';
+            }
+
+            socket.on(params.socketCustomEvent, function(message) {
+                try {
+                    socket.broadcast.emit(params.socketCustomEvent, message);
+                } catch (e) {}
+            });
+        } catch (e) {}
+    });
+}
+
+if (autoRebootServerOnFailure) {
+    // auto restart app on failure
+    var cluster = require('cluster');
+    if (cluster.isMaster) {
+        cluster.fork();
+
+        cluster.on('exit', function(worker, code, signal) {
+            cluster.fork();
+        });
+    }
+
+    if (cluster.isWorker) {
+        runServer();
+    }
+} else {
+    runServer();
+}
