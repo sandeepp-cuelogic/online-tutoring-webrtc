84a9ecc6668c5d6210537ff515482c55f0e890e4
Wasim <wasim.sayyed@cuelogic.com>
2016-11-29 18:24:31 +0530

    Added clear icon image

--- board/controls/navigation.js
+++ board/controls/navigation.js
@@ -12,7 +12,7 @@ DrawingBoard.Control.Navigation = DrawingBoard.Control.extend({
 		var el = '';
 		if (this.opts.back) el += '<button class="drawing-board-control-navigation-back">&larr;</button>';
 		if (this.opts.forward) el += '<button class="drawing-board-control-navigation-forward">&rarr;</button>';
-		if (this.opts.reset) el += '<button class="drawing-board-control-navigation-reset">&times;</button>';
+		if (this.opts.reset) el += '<button class="drawing-board-control-navigation-reset"></button>';
 		this.$el.append(el);
 
 		if (this.opts.back) {
